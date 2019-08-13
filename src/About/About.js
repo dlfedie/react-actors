52a9558f3259a78c7c17273f14cfcfd2ff640412
Dustin Fedie <dlfedie@gmail.com>
2019-08-13 10:16:06 -0500

    fixed formatting in about

--- src/About/About.js
+++ src/About/About.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class About extends Component {
     render() {
         return (
-            <h2>About
-            <p>This improv troupe likes reacting to today's latest trends and compiling their ideas into a stage show made for the modern internet.</p>
-
-            </h2>
+            <div>
+                <h2>About</h2>
+                <p>This improv troupe likes reacting to today's latest trends and compiling their ideas into a stage show made for the modern internet.</p>
+            </div>
 
         )
     }
